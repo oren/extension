0a3b3b187df205a63abfb32ca48eddf2af7ec7de
Oren <orengolan@gmail.com>
2015-12-22 00:15:52 -0800

    injecting to DOM

--- console-log/myscript.js
+++ console-log/myscript.js
@@ -2,25 +2,37 @@
 
 var $ = document.querySelector.bind(document);
 var ENTER = 13;
+var LEFT_CLICK = 0;
+var topNav;
 
 function initContentScript() {
   var searchBotton = $('.lsb');
   searchBotton.addEventListener('click', search);
 
   var searchBox = $('#lst-ib');
-  searchBox.addEventListener('keyup', searchBoxUp);
-  searchBox.addEventListener('keydown', searchBoxDown);
-  searchBox.addEventListener('keypress', searchBoxPress);
+  searchBox.addEventListener('keydown', search);
 
-  function search() {
-    console.log('search db and inject product on top')
-  }
-
-  function searchBoxDown(e) {
-    if (e.keyCode === ENTER) {
-      console.log('search db and inject product on top')
+  function search(e) {
+    if (e.keyCode === ENTER || e.keyCode === LEFT_CLICK) {
+      generateDOM();
     }
   }
+}
+
+function generateDOM() {
+  topNav = $('#top_nav');
+  // TODO: get from DB
+  var products = ['usb stick 4GB', 'usb stick 8GB', 'usb stick 10GB'];
+  var items = '';
+
+  products.forEach(function(item) {
+    items += '<li>' + item + '</li>';
+  });
+
+  var style = 'margin: auto; width: 57%; background-color: lightblue;';
+  var listStyle = 'list-style-type: none;';
+  var html = '<div id="products" style="' + style + '"><ul style="' + listStyle + '">' + items + '</ul></div>';
+  topNav.insertAdjacentHTML('afterend', html);
 }
 
 initContentScript();
