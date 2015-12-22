7de073c1b9bec479e2553cba7ecae228517d4eed
Oren <orengolan@gmail.com>
2015-12-22 01:47:05 -0800

    templates

--- console-log/myscript.js
+++ console-log/myscript.js
@@ -4,8 +4,13 @@ var $ = document.querySelector.bind(document);
 var ENTER = 13;
 var LEFT_CLICK = 0;
 var topNav;
+var itemHtml;
 
 function initContentScript() {
+  getItem(function(html) {
+    itemHtml = html;
+  });
+
   var searchBotton = $('.lsb');
   searchBotton.addEventListener('click', search);
 
@@ -20,19 +25,56 @@ function initContentScript() {
 }
 
 function generateDOM() {
+
   topNav = $('#top_nav');
   // TODO: get from DB
-  var products = ['usb stick 4GB', 'usb stick 8GB', 'usb stick 10GB'];
+  var products = [
+    {
+      title: 'Pendrive de 8gb',
+      link: 'http://tiendas.mediamarkt.es/p/pendrive-de-8gb-sandisk-cruzer-blade-usb-2.0-ultracompacto-color-negro-y-rojo-1117066',
+      price: '5.99'
+    },
+    {
+      title: 'Pendrive de 16gb',
+      link: 'http://tiendas.mediamarkt.es/p/pendrive-de-8gb-sandisk-cruzer-blade-usb-2.0-ultracompacto-color-negro-y-rojo-1117066',
+      price: '8.99'
+    }
+  ];
+
   var items = '';
+  var tmpItem;
 
   products.forEach(function(item) {
-    items += '<li>' + item + '</li>';
+    tmpItem = itemHtml.replace('{TITLE}',item.title);
+    tmpItem = tmpItem.replace('{LINK}',item.link);
+    tmpItem = tmpItem.replace('{PRICE}',item.price);
+    items += tmpItem;
   });
 
-  var style = 'margin: auto; width: 57%; background-color: lightblue;';
+  var style = 'margin-left: 120px;';
   var listStyle = 'list-style-type: none;';
   var html = '<div id="products" style="' + style + '"><ul style="' + listStyle + '">' + items + '</ul></div>';
+  var productsElement = $('#products');
+
+  if (productsElement) {
+    productsElement.remove();
+  }
+
   topNav.insertAdjacentHTML('afterend', html);
+}
+
+function getItem(cb) {
+  var url = chrome.extension.getURL('templates/item.html');
+  var req = new XMLHttpRequest();
+
+  req.open('GET', url, true);
+  req.onreadystatechange = function() {
+    if (req.readyState === 4) {
+      cb(req.responseText);
+    }
+  };
+  req.send(null);
+  return req;
 }
 
 initContentScript();
